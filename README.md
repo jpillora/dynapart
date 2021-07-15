29cd61bc7dcaf3aab74ad7486edf4a613eb6d7c0
Jaime Pillora <dev@jpillora.com>
2021-07-15 13:36:38 +1000

    docs

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # dynapart
 
-A short description of `dynapart`
+A simple command-line interface to DynamoDB using PartiQL
 
 [![GoDev](https://img.shields.io/static/v1?label=godoc&message=reference&color=00add8)](https://pkg.go.dev/github.com/jpillora/dynapart)
 [![CI](https://github.com/jpillora/dynapart/workflows/CI/badge.svg)](https://github.com/jpillora/dynapart/actions?workflow=CI)
 
-### Features
+### Demo
 
-* Simple
-* Single binary
-* Easy to use
+```json
+$ dynapart 'SELECT Author,ID FROM Books'
+{"Author":"Ben Horowitz","ID":"5bv2Fs1JSCiVrDyNEoRkQ"}
+{"Author":"Matthew McConaughey","ID":"x66TMQ3gTf6yileYGQXvg"}
+{"Author":"Ernest Cline","ID":"jxUE0ftHTD6O4ogI2Dh0g"}
+{"Author":"Isaac Asimov","ID":"FVzOUf0WQujIJ1WxX2PhA"}
+{"Author":"Chris Voss, Tahl Raz","ID":"4Pba0gmOStyHrF9N9yMAQ"}
+{"Author":"Terry Pratchett","ID":"zx_CovWDRM6pOPdkkvfzQ"}
+```
 
 ### Install
 
@@ -27,11 +33,58 @@ $ curl https://i.jpillora.com/dynapart! | bash
 **Source**
 
 ```sh
-$ go get -v github.com/jpillora/dynapart
+$ go install -v github.com/jpillora/dynapart@latest
 ```
 
 ### Usage
 
-```go
+<!--tmpl,code=plain:echo "$ dynapart --help" && go run main.go --help -->
+``` plain 
+$ dynapart --help
+
+  Usage: dynapart [options] <statement> [arg] [arg] ...
+
+  Executes the given DynamoDB PartiQL <statement>,
+  which may contain ? placeholders. If placeholders are set,
+  you must provide corresponding [arg]s where each [arg] is valid JSON.
+
+  Options:
+  --max-pages, -m    maximum number of result pages to return (default 1)
+  --consistent-read  enable consistent reads
+  --item-format, -i  json is the only format currently
+  --no-colors, -c    disable json syntax highlighting (default true)
+  --verbose, -v      print actions to stderr
+  --version          display version
+  --help, -h         display help
+
+  For more information on using PartiQL with DynamoDB, see
+  • https://partiql.org/tutorial.html
+  • https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/ql-reference.html
+
+  Each DynamoDB item is written to stdout as JSON, one object per line, which makes
+  dynapart jq compatible.
+
+  Syntax highlighting is automatically enabled when stdout is a TTY.
+  This can be overridden with --no-colors=true/false.
+
+  The default AWS SDK authorization process is performed on program start.
+  Basically, AWS environment variables will be used, followed by a profile (AWS_PROFILE),
+  followed by the EC2 metadata endpoint. See AWS documentation for details.
+
+  You can use a special statement "SHOW TABLES" to list your DynamoDB tables.
+  Note, This is not valid PartiQL.
+
+  Version:
+    0.0.0-src
+
+  Read more:
+    https://github.com/jpillora/dynapart
 
 ```
+<!--/tmpl-->
+
+### TODO
+
+* Match JSON key order with SELECT
+* CSV output
+* Custom AWS authorization
--- main.go
+++ main.go
@@ -22,14 +22,39 @@ import (
 	"github.com/mattn/go-isatty"
 )
 
+const summary = `
+Executes the given DynamoDB PartiQL <statement>,
+which may contain ? placeholders. If placeholders are set,
+you must provide corresponding [arg]s where each [arg] is valid JSON.
+`
+
+const extra = `
+For more information on using PartiQL with DynamoDB, see
+• https://partiql.org/tutorial.html
+• https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/ql-reference.html
+
+Each DynamoDB item is written to stdout as JSON, one object per line, which makes
+dynapart jq compatible.
+
+Syntax highlighting is automatically enabled when stdout is a TTY.
+This can be overridden with --no-colors=true/false.
+
+The default AWS SDK authorization process is performed on program start.
+Basically, AWS environment variables will be used, followed by a profile (AWS_PROFILE),
+followed by the EC2 metadata endpoint. See AWS documentation for details.
+
+You can use a special statement "SHOW TABLES" to list your DynamoDB tables.
+Note, This is not valid PartiQL.
+`
+
 var version = "0.0.0-src"
 
 type cli struct {
-	MaxPages       int      `opts:"help=maximum number of result patches to return"`
+	MaxPages       int      `opts:"help=maximum number of result pages to return"`
 	ConsistentRead bool     `opts:"help=enable consistent reads"`
 	ItemFormat     string   `opts:"help=json is the only format currently"`
-	NoItemNumber   bool     `opts:"short=n, help=hide item numbers"`
 	NoColors       bool     `opts:"short=c, help=disable json syntax highlighting"`
+	Verbose        bool     `opts:"help=print actions to stderr"`
 	Statement      string   `opts:"mode=arg"`
 	Args           []string `opts:"mode=arg"`
 }
@@ -41,17 +66,19 @@ func main() {
 }
 
 func run() error {
+	plain := !isatty.IsTerminal(os.Stdout.Fd())
 	//execute cli
 	c := cli{
 		MaxPages: 1,
-		NoColors: !isatty.IsTerminal(os.Stdout.Fd()),
+		NoColors: plain,
 	}
 	opts.
 		New(&c).
+		Name("dynapart").
 		Version(version).
-		DocAfter("usage", "more", "\nExecutes the given DynamoDB PartiQL <statement>,\n"+
-			"which may contain ? placeholders. If placeholders are set,\n"+
-			"you must provide corresponding [arg]s where each [arg] is valid JSON.\n").
+		DocAfter("usage", "more", summary).
+		DocBefore("version", "extra", extra).
+		Repo("https://github.com/jpillora/dynapart").
 		Parse()
 	//convert args to json then to dynamodb params
 	var params attrs
@@ -66,6 +93,11 @@ func run() error {
 		}
 		params = append(params, attr)
 	}
+	//setup printer
+	p := printer{
+		colors: !c.NoColors,
+		verb:   c.Verbose,
+	}
 	//setup aws session
 	sess, err := session.NewSession()
 	if err != nil {
@@ -77,6 +109,7 @@ func run() error {
 	ctx := interruptibleContext()
 	//magic string "SHOW TABLES"
 	if strings.ToUpper(c.Statement) == "SHOW TABLES" {
+		p.verbf("execute list tables...\n")
 		return db.ListTablesPagesWithContext(ctx, &dynamodb.ListTablesInput{}, func(lto *dynamodb.ListTablesOutput, b bool) bool {
 			for _, t := range lto.TableNames {
 				fmt.Println(*t)
@@ -84,13 +117,12 @@ func run() error {
 			return true
 		})
 	}
-	//
-	printer := printer{colors: !c.NoColors, nums: !c.NoItemNumber}
 	//execute
 	var next *string
 	page := 1
 	t0 := time.Now()
 	for {
+		p.verbf("execute statement: %s\n", c.Statement)
 		out, err := db.ExecuteStatementWithContext(ctx, &dynamodb.ExecuteStatementInput{
 			Statement:  &c.Statement,
 			Parameters: params,
@@ -99,14 +131,20 @@ func run() error {
 		if err != nil {
 			return err
 		}
-		printer.items(out.Items)
-		if out.NextToken == nil || page == c.MaxPages {
+		p.items(out.Items)
+		if out.NextToken == nil {
+			p.verbf("no more items\n")
 			break
 		}
+		if page == c.MaxPages {
+			p.verbf("hit max page %d\n", page)
+			break
+		}
+		p.verbf("page %d contained %d items, has more items...", page, len(out.Items))
 		next = out.NextToken
 		page++
 	}
-	fmt.Printf("[returned %d items in %s]\n", printer.n, time.Since(t0))
+	p.verbf("returned %d items in %s]\n", p.n, time.Since(t0))
 	return nil
 }
 
@@ -119,9 +157,8 @@ type item = map[string]*attr
 type items = []item
 
 type printer struct {
-	n      int
-	nums   bool
-	colors bool
+	n            int
+	verb, colors bool
 }
 
 func (p *printer) items(items items) {
@@ -139,14 +176,19 @@ func (p *printer) item(item item) {
 		s = highlight(s)
 	}
 	p.n++
-	if p.nums {
-		fmt.Printf("[#%d] ", p.n)
+	if p.verb {
+		fmt.Fprintf(os.Stderr, "[#%d] ", p.n)
 	}
 	fmt.Println(s)
 }
 
+func (p *printer) verbf(format string, args ...interface{}) {
+	if p.verb {
+		fmt.Fprintf(os.Stderr, ">>> "+format, args...)
+	}
+}
+
 func interruptibleContext() context.Context {
-	//allow user to gracefully shutdown with Ctrl+C
 	ctx, cancel := context.WithCancel(context.Background())
 	go func() {
 		count := 0
